1760a4e06b5cb95ea6fa0ed41cf79beedb1b55ee
ash-047 <ashfaq.am220@gmail.com>
2025-04-07 09:23:29 +0530

    Branch Pull error

--- src/main/java/com/airline/management/service/payment/PaymentProcessor.java
+++
@@ -1,9 +0,0 @@
-package com.airline.management.service.payment;
-
-import com.airline.management.model.Payment;
-
-// Strategy Pattern: Interface for different payment processing strategies
-public interface PaymentProcessor {
-    Payment process(Payment payment);
-    Payment refund(Payment payment);
-}
--- src/main/java/com/airline/management/util/NotificationObserver.java
+++
@@ -1,9 +0,0 @@
-package com.airline.management.util;
-
-// Observer Pattern: Observer interface that gets notified of events
-public interface NotificationObserver {
-    void onBookingConfirmation(Long bookingId, String email);
-    void onBookingCancellation(Long bookingId, String email);
-    void onFlightStatusChange(Long flightId, String status);
-    void onEnquiryResponse(Long enquiryId, String email);
-}
